a1f4108ec19bb788b5f5040e4630fc863b9bb871
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-07-17 15:27:49 +0700

    Fix message typo

--- test/generic_compare_buffer_test.js
+++ test/generic_compare_buffer_test.js
@@ -4,7 +4,7 @@ const filesig = require('../index');
 const signature = require('../lib/signature');
 
 describe('Test Generic Compare Buffer Validation', () => {
-  const validSampleBuffer = Buffer.from([0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52]);
+  const validSampleBuffer = Buffer.from([0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52]); // Mock using png data
   const invalidSampleBuffer = Buffer.from([]);
 
   it('should return false when input buffer is invalid', () => {
@@ -12,7 +12,7 @@ describe('Test Generic Compare Buffer Validation', () => {
     assert.equal(valid, false);
   });
 
-  it('should return false when input buffer is invalid', () => {
+  it('should return true when input buffer is valid', () => {
     const valid = filesig.genericCompareBuffer(validSampleBuffer, signature.PNG);
     assert.equal(valid, true);
   });
