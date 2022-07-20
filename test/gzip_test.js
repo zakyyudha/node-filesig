066585cb4886f9c3761c40d8c2043a6fab3fe568
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-07-20 10:34:16 +0700

    Formatter

--- test/gzip_test.js
+++ test/gzip_test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
+const fs = require('fs');
 const filesig = require('../index');
-const fs = require("fs");
 
 describe('Test GZIP Validation', () => {
   it('should return false when input buffer is invalid GZIP', () => {
