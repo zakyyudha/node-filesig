78bf7a06a4b5c31f831ee27f16b0ddf94feb0d1a
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-07-20 12:32:19 +0700

    Add buffer test validation

---
+++ test/buffer_test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const filesig = require('../index');
+
+describe('Test Buffer Validation', () => {
+  const emptyBufferSample = ""; // mock using empty string
+  it('should throw error if buffer is invalid', () => {
+    const valid = () => {
+      filesig.isJpeg(emptyBufferSample)
+    };
+    assert.throws(valid, Error);
+  });
+});
