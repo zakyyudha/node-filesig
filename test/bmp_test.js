41e33aaeb97254aebec23ffa8274e98b92c4ebd9
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-07-20 09:29:43 +0700

    Update bmp test using real file

--- test/bmp_test.js
+++ test/bmp_test.js
@@ -1,18 +1,21 @@
 const assert = require('assert');
 const { Buffer } = require('buffer');
 const filesig = require('../index');
+const fs = require("fs");
 
 describe('Test BMP Validation', () => {
-  const validBmpBuffer = Buffer.from([0x42, 0x4D, 0x8A, 0x7B, 0x0C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x8A, 0x00, 0x00, 0x00, 0x7C, 0x00]);
-  const invalidBmpBuffer = Buffer.from([0x41, 0x4D, 0x8A, 0x7B, 0x0C, 0x00, 0x00, 0x00, 0x00, 0x00, 0x8A, 0x00, 0x00, 0x00, 0x7C, 0x00]);
-
   it('should return false when input buffer is invalid BMP', () => {
-    const valid = filesig.isBmp(invalidBmpBuffer);
-    assert.equal(valid, false);
+    fs.readFile('./tmp/sample-0.svg', (error, invalidBmpBuffer) => {
+      if (error) throw error;
+      const valid = filesig.isBmp(invalidBmpBuffer);
+      assert.equal(valid, false);
+    });
   });
-
   it('should return true when input buffer is valid BMP', () => {
-    const valid = filesig.isBmp(validBmpBuffer);
-    assert.equal(valid, true);
+    fs.readFile('./tmp/sample-0.bmp', (error, validBmpBuffer) => {
+      if (error) throw error;
+      const valid = filesig.isBmp(validBmpBuffer);
+      assert.equal(valid, true);
+    });
   });
 });
