e841df730a3258c41bb8795f54c65affaf1fa0a6
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-07-20 11:48:14 +0700

    Add mpg test validation

---
+++ test/mpg_test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const fs = require('fs');
+const filesig = require('../index');
+
+describe('Test MPG Validation', () => {
+  it('should return false when input buffer is invalid MPG', () => {
+    fs.readFile('./tmp/sample-0.3gp', (error, invalidMpgBuffer) => {
+      if (error) throw error;
+      const valid = filesig.isMpg(invalidMpgBuffer);
+      assert.equal(valid, false);
+    });
+  });
+  it('should return true when input buffer is valid MPG', () => {
+    fs.readFile('./tmp/sample-0.mpg', (error, validMpgBuffer) => {
+      if (error) throw error;
+      const valid = filesig.isMpg(validMpgBuffer);
+      assert.equal(valid, true);
+    });
+  });
+});
