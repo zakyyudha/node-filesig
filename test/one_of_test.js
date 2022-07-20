8d4e809671f4836860ac3f556b3ce3d7077e4601
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-07-20 12:23:23 +0700

    Add some unit test on one_of

--- test/one_of_test.js
+++ test/one_of_test.js
@@ -19,6 +19,24 @@ describe('Test One Of Validation', () => {
     });
   });
 
+  it('should return false when input buffer is valid, but one of the proper functions supplied is not available', () => {
+    fs.readFile('./tmp/sample-0.png', (error, validPngBuffer) => {
+      if (error) throw error;
+      const valid = filesig.oneOf(validPngBuffer, filesig.is3gp, filesig.isApk, filesig.isZip, filesig.isGif);
+      assert.equal(valid, false);
+    });
+  });
+
+  it('should throw error when some functions are invalid', () => {
+    fs.readFile('./tmp/sample-0.jpg', (error, invalidPngBuffer) => {
+      if (error) throw error;
+      const valid = () => {
+        filesig.oneOf(invalidPngBuffer, filesig.is3gp, filesig.isApk, filesig.isPng, filesig.isRandomFunction, filesig.isGif)
+      };
+      assert.throws(valid, Error);
+    });
+  })
+
   it('should return false when input buffer is invalid', () => {
     fs.readFile('./tmp/sample-0.jpg', (error, invalidPngBuffer) => {
       if (error) throw error;
