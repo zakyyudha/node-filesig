5050d1d1e24eebaefcbab02bed640b6dda76ba82
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-07-20 11:47:38 +0700

    Add dib test validation

---
+++ test/dib_test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const fs = require('fs');
+const filesig = require('../index');
+
+describe('Test DIB Validation', () => {
+  it('should return false when input buffer is invalid DIB', () => {
+    fs.readFile('./tmp/sample-0.jpg', (error, invalidDibBuffer) => {
+      if (error) throw error;
+      const valid = filesig.isDib(invalidDibBuffer);
+      assert.equal(valid, false);
+    });
+  });
+  it('should return true when input buffer is valid DIB', () => {
+    fs.readFile('./tmp/sample-0.dib', (error, validDibBuffer) => {
+      if (error) throw error;
+      const valid = filesig.isDib(validDibBuffer);
+      assert.equal(valid, true);
+    });
+  });
+});
