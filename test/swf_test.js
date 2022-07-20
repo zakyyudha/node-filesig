f1f860349249262b8b526ddeb7a6378a65138832
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-07-20 11:48:37 +0700

    Add swf test validation

---
+++ test/swf_test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const fs = require('fs');
+const filesig = require('../index');
+
+describe('Test SWF Validation', () => {
+  it('should return false when input buffer is invalid SWF', () => {
+    fs.readFile('./tmp/sample-0.3gp', (error, invalidSwfBuffer) => {
+      if (error) throw error;
+      const valid = filesig.isSwf(invalidSwfBuffer);
+      assert.equal(valid, false);
+    });
+  });
+  it('should return true when input buffer is valid SWF', () => {
+    fs.readFile('./tmp/sample-0.swf', (error, validSwfBuffer) => {
+      if (error) throw error;
+      const valid = filesig.isSwf(validSwfBuffer);
+      assert.equal(valid, true);
+    });
+  });
+});
