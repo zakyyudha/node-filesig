6182d456099fe7aca96442158641e665f60e4247
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-07-20 09:06:37 +0700

    Update mp4 test using real file

--- test/mp4_test.js
+++ test/mp4_test.js
@@ -1,7 +1,6 @@
 const assert = require('assert');
-const { Buffer } = require('buffer');
+const fs = require('fs');
 const filesig = require('../index');
-const fs = require('fs')
 
 describe('Test MP4 Validation', () => {
   it('should return false when input buffer is invalid MP4', () => {
@@ -9,13 +8,13 @@ describe('Test MP4 Validation', () => {
       if (error) throw error;
       const valid = filesig.isMp4(invalidMp4Buffer);
       assert.equal(valid, false);
-    })
+    });
   });
   it('should return true when input buffer is valid MP4', () => {
     fs.readFile('./tmp/sample-0.mp4', (error, validMp4Buffer) => {
       if (error) throw error;
       const valid = filesig.isMp4(validMp4Buffer);
       assert.equal(valid, true);
-    })
+    });
   });
 });
