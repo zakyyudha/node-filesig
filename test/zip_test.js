0dbc16be250361ea58ce64791833ddac19b9d801
Lutfa Ibtihaji Ilham <lutfailham96@gmail.com>
2022-07-20 10:33:30 +0700

    Update zip test using real file

--- test/zip_test.js
+++ test/zip_test.js
@@ -1,18 +1,21 @@
 const assert = require('assert');
-const { Buffer } = require('buffer');
+const fs = require('fs');
 const filesig = require('../index');
 
 describe('Test ZIP Validation', () => {
-  const validZipBuffer = Buffer.from([0x50, 0x4B, 0x03, 0x04, 0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x88, 0xB4, 0x5E, 0x4B, 0x00, 0x00]);
-  const invalidZipBuffer = Buffer.from([0x52, 0x4B, 0x03, 0x04, 0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x88, 0xB4, 0x5E, 0x4B, 0x00, 0x00]);
-
   it('should return false when input buffer is invalid ZIP', () => {
-    const valid = filesig.isZip(invalidZipBuffer);
-    assert.equal(valid, false);
+    fs.readFile('./tmp/sample-0.rar', (error, invalidZipBuffer) => {
+      if (error) throw error;
+      const valid = filesig.isZip(invalidZipBuffer);
+      assert.equal(valid, false);
+    });
   });
 
   it('should return true when input buffer is valid ZIP', () => {
-    const valid = filesig.isZip(validZipBuffer);
-    assert.equal(valid, true);
+    fs.readFile('./tmp/sample-0.zip', (error, validZipBuffer) => {
+      if (error) throw error;
+      const valid = filesig.isZip(validZipBuffer);
+      assert.equal(valid, true);
+    });
   });
 });
